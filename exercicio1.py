56ff58d8a64685db25eaa8f661327bcc3b517d89
MatheusNery005 <164574641+MatheusNery005@users.noreply.github.com>
2024-03-25 21:00:15 -0300

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,7 @@
+numero = int(input("Digite o número: "))
+if (numero % 3 == 0) and (numero % 5 == 0){
+        print("o numero que voce digitou é(",numero,")e é divisivel por 3 e 5 ao mesmo tempo")
+}
+    else
+        print("o numero (",numero,")não é divisivel por 3 e 5");
+
